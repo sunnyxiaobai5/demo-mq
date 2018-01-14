847aba0e19fc6c8ce8cb7750f29d882324bf3167
xiangyong zeng <zengxiangyong@haixue.com>
2018-01-14 23:37:01 +0800

    change code sequence

--- rabbitmq/src/main/java/com/sunnyxiaobai5/demo/rabbitmq/java/tutorial1/Send.java
+++ rabbitmq/src/main/java/com/sunnyxiaobai5/demo/rabbitmq/java/tutorial1/Send.java
@@ -14,8 +14,6 @@ public class Send {
     private static final String QUEUE_NAME = "queue-tutorial1";
 
     public static void main(String[] args) throws IOException, TimeoutException {
-        String message = "hello world!";
-
         ConnectionFactory factory = new ConnectionFactory();
 
         factory.setUsername("admin");
@@ -28,6 +26,7 @@ public class Send {
 
         channel.queueDeclare(QUEUE_NAME, false, false, false, null);
 
+        String message = "hello world!";
         for (int i = 0; i < 100; i++) {
             channel.basicPublish("", QUEUE_NAME, null, (message + i).getBytes("UTF-8"));
             System.out.println("send message " + message + i);
